65c8d73e2bd4f58a6bac80cf688fe00f8a4695c0
Hugo Arredondo <hugosao22@gmail.com>
2020-08-22 20:01:58 -0400

    modify program

--- piedra_papel_tijera.rb
+++ piedra_papel_tijera.rb
@@ -28,60 +28,74 @@ puts
 puts menu1
 
 jugador1 = gets.chomp.to_i
-
-while jugador1 != 4
-    if jugador1 == 1 || jugador1 == 2 || jugador1 == 3
-        op1 = jugador1
-        break
-    else
-        puts
-        puts "No existe esa opción. Escoja otra vez."
-        puts
-        puts menu1
-        jugador1 = gets.chomp.to_i
+if jugador1 >= 1 && jugador1 < 4
+    while jugador1 != 4
+        if jugador1 == 1 || jugador1 == 2 || jugador1 == 3
+            op1 = jugador1
+            break
+        else
+            puts
+            puts "No existe esa opción. Escoja otra vez."
+            puts
+            puts menu1
+            jugador1 = gets.chomp.to_i
+        end
     end
-end
 
-puts
-puts "\t-------------------"
-puts "\t| Segundo Jugador |"
-puts "\t-------------------"
-puts
-puts "Eliga una opción (seleccione el número de la opción)"
-puts
-puts menu2
+    puts
+    puts "\t-------------------"
+    puts "\t| Segundo Jugador |"
+    puts "\t-------------------"
+    puts
+    puts "Eliga una opción (seleccione el número de la opción)"
+    puts
+    puts menu2
 
-jugador2 = gets.chomp.to_i
+    jugador2 = gets.chomp.to_i
 
-while jugador2 != 4
-    if jugador2 == 1 || jugador2 == 2 || jugador2 == 3
-        op2 = jugador2
-        break
+    if jugador2 >= 1 && jugador2 < 4
+
+        while jugador2 != 4
+            if jugador2 == 1 || jugador2 == 2 || jugador2 == 3
+                op2 = jugador2
+                break
+            else
+                puts
+                puts "No existe esa opción. Escoja otra vez."
+                puts
+                puts menu2
+                jugador2 = gets.chomp.to_i
+            end
+        end
     else
         puts
-        puts "No existe esa opción. Escoja otra vez."
-        puts
-        puts menu2
-        jugador2 = gets.chomp.to_i
+        puts "\t---------------------------------"
+        puts "\tSegundo jugador salió de Cachipun"
+        puts "\t---------------------------------"
     end
-end
 
-case jugador1
-    when 1
-        puts
-        puts "Empataron" if jugador2 == 1
-        puts "Primer jugador gana" if jugador2 == 3
-        puts "Segundo jugador gana" if jugador2 == 2
-    when 2
-        puts
-        puts "Empataron" if jugador2 == 2
-        puts "Primer jugador gana" if jugador2 == 1
-        puts "Segundo jugador gana" if jugador2 == 3
-    else
-        puts
-        puts "Empataron" if jugador2 == 3
-        puts "Primer jugador gana" if jugador2 == 2
-        puts "Segundo jugador gana" if jugador2 == 1
+    case jugador1
+        when 1
+            puts
+            puts "Empataron" if jugador2 == 1
+            puts "Primer jugador gana" if jugador2 == 3
+            puts "Segundo jugador gana" if jugador2 == 2
+        when 2
+            puts
+            puts "Empataron" if jugador2 == 2
+            puts "Primer jugador gana" if jugador2 == 1
+            puts "Segundo jugador gana" if jugador2 == 3
+        else
+            puts
+            puts "Empataron" if jugador2 == 3
+            puts "Primer jugador gana" if jugador2 == 2
+            puts "Segundo jugador gana" if jugador2 == 1
+    end
+else
+    puts
+    puts "\t--------------------------------"
+    puts "\tPrimer jugador salió de Cachipun"
+    puts "\t--------------------------------"
 end
 
 puts
